24f93d5abb0d47f7df2937b3c39072cea51c0904
Tony Bradley <antbradley91@gmail.com>
2017-08-05 17:35:21 -0400

    wip

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const m = require('mithril')
 const css = require('sheetify')
+const clientId = require('shortid').generate()
 const store = require('./store')
 const home = require('./modules/home')
 
@@ -25,17 +26,22 @@ function startApp(err) {
 	}
 }
 
-if (process.env.NODE_ENV === 'development') {
-	// Wrap request to hit local host
-	m.request = (function (mRequest) {
-		return function (opts) {
-			if (opts.url[0] === '/') {
-				opts.url = 'http://localhost:5000' + opts.url
-			}
-			return mRequest.call(m, opts)
-		}
-	})(m.request)
+const development = process.env.NODE_ENV === 'development'
 
+// Wrap request to hit local host
+m.request = (function (mRequest) {
+	return function (opts) {
+		if (opts.url[0] === '/' && development) {
+			opts.url = 'http://localhost:5000' + opts.url
+		}
+		if (opts.data && opts.data.mutation) {
+			opts.data.clientId = clientId
+		}
+		return mRequest.call(m, opts)
+	}
+})(m.request)
+
+if (development) {
 	// Enable state persist then start app
 	require('./utils/persist')(appName, store, startApp)
 } else {
